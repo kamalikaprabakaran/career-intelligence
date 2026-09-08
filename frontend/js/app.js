560d9bde8872697c562de433a81cafc4af7d66c9
YourGitHubUsername <your-email@example.com>
2026-09-08 10:40:15 +0530

    port link changing fro both frontend and backend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,5 +1,5 @@
 // Change this if your backend runs somewhere else (e.g. your Render URL later)
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://127.0.0.1:8001";
 
 const dbStatusEl = document.getElementById("db-status");
 const formEl = document.getElementById("user-form");
